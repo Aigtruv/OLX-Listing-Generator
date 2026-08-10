d2a00ccb10513cd0d9cb99a4eea415980b368e78
almaz.b <almaz.b@halykmarket.com>
2026-08-10 15:18:44 +0500

    feat: add AiAgentPublisherApplication

---
+++ src/test/java/com/example/aiagentpublisher/AiAgentPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiagentpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
